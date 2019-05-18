767560e679aa82101755e420fd5bd1f00e3824f1
Sifan Jiang <jasper.sifan@gmail.com>
2019-05-19 00:09:17 +0800

    Finish h1-ex1

--- Homework/h1/MATLAB/main.m
+++ Homework/h1/MATLAB/main.m
@@ -1,4 +1,8 @@
 close all; clear; clc;
 
-a = 30030; b = 257;
-[r1, s1, t1] = extendedEuclidean(a, b);
+% a = 30030; b = 257;
+% [r1, s1, t1] = extendedEuclidean(a, b);
+
+syms n
+eqn = n * log(n) == 128^2;
+soln = solve(eqn, n);

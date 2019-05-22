bbd8cd769c890623ba7412d3dbda7ba34d451760
JasperRice <jasper.sifan@gmail.com>
2019-05-22 14:23:38 +0800

    README unfinished.

--- Homework/h1/CPP/main.cpp
+++ Homework/h1/CPP/main.cpp
@@ -1,14 +1,13 @@
 #include <ctime>
 #include <gmpxx.h>
 #include <iostream>
-#include <utility>
 using namespace std;
 
-struct pair_s {
+typedef struct pair_s {
 	pair<mpz_class, mpz_class> R;
 	pair<mpz_class, mpz_class> S;
 	pair<mpz_class, mpz_class> T;
-};
+} pair_s;
 
 pair_s extendedEuclidean(const mpz_class& a, const mpz_class& b) {
 	pair_s P;
@@ -17,13 +16,13 @@ pair_s extendedEuclidean(const mpz_class& a, const mpz_class& b) {
 	pair<mpz_class, mpz_class> T = make_pair(1, 0);
 	while (R.first != 0) {
 		mpz_class q = R.second / R.first;
-		R = make_pair(R.first, R.second - q * R.first);
-		S = make_pair(S.first, S.second - q * S.first);
-		T = make_pair(T.first, T.second - q * T.first);
+		R = make_pair(R.second - q * R.first, R.first);
+		S = make_pair(S.second - q * S.first, S.first);
+		T = make_pair(T.second - q * T.first, T.first);
 	}
 	P.R = R;
-	R.S = S;
-	R.T = T;
+	P.S = S;
+	P.T = T;
 	return P;
 }
 
@@ -33,16 +32,20 @@ int main(int argc, char const *argv[]) {
 	randclass.seed((unsigned long) time(nullptr));
 
 	// Generate a and b
-	mpz_class a, b, r;
+	mpz_class a, b, r, r_lib;
 	a = randclass.get_z_bits(4096);
 	b = randclass.get_z_bits(4096);
 
 	pair_s P = extendedEuclidean(a, b);
-	mpz_class r1 = P.R.first;
-	mpz_class r1_lib = gcd(a, b);
+	r = P.R.second;
+	r_lib = gcd(a, b);
 
-	std::cout << "A: " << a << "; B: " << b << ".\n";
-	if (r1 == r1_lib) cout << "The results are same.\n";
+	// Show a, b, and results
+	cout << "A: " << a
+		<< "\nB: " << b
+		<< "\ngcd(a,b): " << r
+		<< "\nlibgcd(a,b): " << r_lib << '\n';
+	if (r == r_lib) cout << "The results are same.\n";
 	else cout << "The results are different.\n";
 	return 0;
 }

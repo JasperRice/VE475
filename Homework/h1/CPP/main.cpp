6ba8312411fd1eefca017e234f0949d235fa4ca4
JasperRice <jasper.sifan@gmail.com>
2019-05-22 05:13:46 +0200

    Working on h1-ex3.

--- Homework/h1/CPP/main.cpp
+++ Homework/h1/CPP/main.cpp
@@ -1,51 +1,48 @@
 #include <ctime>
+#include <gmpxx.h>
 #include <iostream>
 #include <utility>
 using namespace std;
 
-pair_s extendedEuclidean(const mpz_class& a, const mpz_class& b);
-
 struct pair_s {
-	pair<mpz_class> R;
-	pair<mpz_class> S;
-	pair<mpz_class> T;
+	pair<mpz_class, mpz_class> R;
+	pair<mpz_class, mpz_class> S;
+	pair<mpz_class, mpz_class> T;
 };
-
-int main(int argc, char const *argv[]) {
-	// Random seed here
-
-	mpz_class a, b;
-	a = randclass.get_z_bits(4096);
-	b = randclass.get_z_bits(4096);
-	pair_s P = extendedEuclidean(a, b);
-
-	????? = gcd(a, b);
-
-	mpz_class r1 = P.R.?????;
-
-	if (????? == ?????) {
-		/* code */
-	}
-	else {
-
-	}
-
-	return 0;
-}
 
 pair_s extendedEuclidean(const mpz_class& a, const mpz_class& b) {
 	pair_s P;
-	pair R = make_pair(a, b);
-	pair S = make_pair(0, 1);
-	pair T = make_pair(1, 0);
-	while (R.????? != 0) {
-		mpz_class q = R.1????? / R.0?????;
-		R = make_pair(R.0?????, R.1????? - q * R.0?????);
-		S = make_pair(S.0?????, S.1????? - q * S.0?????);
-		T = make_pair(T.0?????, T.1????? - q * T.0?????);
+	pair<mpz_class, mpz_class> R = make_pair(a, b);
+	pair<mpz_class, mpz_class> S = make_pair(0, 1);
+	pair<mpz_class, mpz_class> T = make_pair(1, 0);
+	while (R.first != 0) {
+		mpz_class q = R.second / R.first;
+		R = make_pair(R.first, R.second - q * R.first);
+		S = make_pair(S.first, S.second - q * S.first);
+		T = make_pair(T.first, T.second - q * T.first);
 	}
 	P.R = R;
 	R.S = S;
 	R.T = T;
 	return P;
+}
+
+int main(int argc, char const *argv[]) {
+	// Generate random seed
+	gmp_randclass randclass(gmp_randinit_default);
+	randclass.seed((unsigned long) time(nullptr));
+
+	// Generate a and b
+	mpz_class a, b, r;
+	a = randclass.get_z_bits(4096);
+	b = randclass.get_z_bits(4096);
+
+	pair_s P = extendedEuclidean(a, b);
+	mpz_class r1 = P.R.first;
+	mpz_class r1_lib = gcd(a, b);
+
+	std::cout << "A: " << a << "; B: " << b << ".\n";
+	if (r1 == r1_lib) cout << "The results are same.\n";
+	else cout << "The results are different.\n";
+	return 0;
 }

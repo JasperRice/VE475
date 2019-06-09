108948ba0c3354b3695552a3f5958ff9cef5e1be
JasperRice <jasperrice@jasper.sifan@gmail.com>
2019-06-09 17:38:20 +0800

    working on ex4

---
+++ Homework/h3/C/aes.h
@@ -0,0 +1,35 @@
+#ifndef AES_H
+#define AES_H
+
+#include <memory.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+class AES {
+private:
+	unsigned char SBox[256];
+	unsigned char InvSBox[256];
+	void GenerateSBox(); // !!!!!
+	void SubBytes(unsigned char state[][4]);
+	void InvSubBytes(unsigned char state[][4]);
+
+    void ShiftRows(unsigned char state[][4]);
+    void InvShiftRows(unsigned char state[][4]);
+
+    unsigned char Multiplication(unsigned char a, unsigned char b); // !!!!!
+    void MixColumns(unsigned char state[][4]);
+    void InvMixColumns(unsigned char state[][4]);
+
+    unsigned char rKey[11][4][4];
+    void GenerateRoundKey(unsigned char* key); // !!!!!
+    void AddRoundKey(unsigned char state[][4], unsigned char k[][4]);
+	
+public:
+	AES(unsigned char* key);
+	virtual ~AES();
+	unsigned char* Encrypt(unsigned char* input);
+	unsigned char* Decrypt(unsigned char* input);
+}
+
+#endif
---
+++ Homework/h3/C/main.c
@@ -0,0 +1,10 @@
+#include <aes.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	unsigned char key[][] = ;
+	unsigned char plaintext[] = ;
+	AES system(key);
+	return 0;
+}
